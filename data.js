9465d667ecfc65d6ea9e528da6492f61ea6aeddd
Aelkoulali <abdellah.elkoulali@gmx.fr>
2023-09-23 11:29:20 +0200

    Add Data

---
+++ data.js
@@ -0,0 +1,31 @@
+const articles = [
+    {
+      id: 1,
+      title: 'Impressive Start',
+      date: new Date(2023, 9, 21,),
+      length: 11,
+      snippet: `The Dallas Cowboys have already 2 wins,The Cowboys' offense, and defense, have been explosive, and impressive.`,
+    },
+    {
+      id: 2,
+      title: 'Bengales Struggles',
+      date: new Date(2023, 9, 16),
+      length: 5,
+      snippet: `Two weeks into the season and things are not going quite as the Bengals planned, as they are 0-2 and far from looking like a Super Bowl-caliber team.`,
+    },
+    {
+      id: 3,
+      title: 'Aaron Rodgers injury',
+      date: new Date(2023, 9, 10),
+      length: 5,
+      snippet: `Jets QB out for season after MRI reveals complete tear of star's Achilles tendon. However, New York's football hopes and dreams have been put on hold as Rodgers is out for the 2023 season.`,
+    },
+    {
+      id: 4,
+      title: 'Hall Of Fame',
+      date: new Date(2023, 8, 3),
+      length: 5,
+      snippet: `DeMarcus Ware Elected to Hall of Fame Class of 2023. Ware was voted first-team All-Pro four times (2007-09, 2011), second-team All-Pro three times (2006, 2010, 2012) and was selected to nine Pro Bowls (2007-2013, 2015-16). He is a member of the NFL’s All-Decade Team of the 2000s.`,
+    },
+  ];
+  
